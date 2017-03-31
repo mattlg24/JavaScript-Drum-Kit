474d2da6909c4d07733ffd6ccf78b992c1a98b87
Matt Gardner <matthewlgardner@gmail.com>
2017-03-31 00:21:52 -0600

    add window.onload to app.js

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  if (!audio) {
-    return //stop function from running all together
-  }
-  audio.currentTime = 0 //rewind to the start
-  audio.play()
-  key.classList.add('playing')
-})
+window.onload = function() {
 
-function removeTransition(e) {
-  console.log(e);
-  // if (e.propertyName !== 'transform') return;
-  //   e.target.classList.remove('playing');
+    window.addEventListener('keydown', function(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+        if (!audio) {
+            return //stop function from running all together
+        }
+        audio.currentTime = 0 //rewind to the start
+        audio.play()
+        key.classList.add('playing')
+    })
+
+    function removeTransition(e) {
+        console.log(e);
+        // if (e.propertyName !== 'transform') return;
+        //   e.target.classList.remove('playing');
+    }
+
+    const keys = document.querySelectorAll('.key')
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
 }
-
-const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
